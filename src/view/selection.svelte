2f7cb18e51e61eeaf0aaca5f6bf1b6a4e8f0481e
Patrick Kranz <patrick-kranz@live.de>
2023-04-10 18:02:55 +0200

    make interactions more discovarable

--- src/view/selection.svelte
+++ src/view/selection.svelte
@@ -8,20 +8,30 @@
 	}
 </script>
 
-<div class="click hide-on-print" on:click={() => next()}>
+<button class="click hide-on-print" on:click={() => next()}>
 	<div>
 		{states[currentState]}
 	</div>
-</div>
+</button>
 
-<style>
+<style lang="scss">
 	.click {
+        background-color: transparent;
+        border: 1px solid transparent;
+        padding: 0px;
 		font-size: 100%;
 		display: grid;
 		justify-items: center;
 		align-items: center;
 		width: 100%;
 		height: 100%;
+
+        &:hover{
+            border: 1px solid var(--character-background-color);
+        }
+        &:active{
+            background-color: var(--character-background-color);
+        }
 	}
 
 	:global(.touchTarget) .click {
